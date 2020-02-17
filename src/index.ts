3a0df5c37d28d680344c2c706de942e4064720c1
David Liu <david.liu@cognite.com>
2020-02-17 16:05:37 -0500

    More exports available

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import "./styles.css";
 export { default as ReactPictureAnnotation } from "./ReactPictureAnnotation";
 export { default as DefaultInputSection } from "./DefaultInputSection";
+export * from "./Shape";
+export * from "./Annotation";
