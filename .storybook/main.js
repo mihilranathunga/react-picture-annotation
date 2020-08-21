faaed0034d5655c7e6983c8ded6da37a8d1f5335
David Liu <david.liu@cognite.com>
2020-08-21 13:17:09 +0200

    feat: semantic releaseD

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,43 +2,10 @@ const path = require('path');
 
 module.exports = {
   stories: ['../stories/*.stories.tsx'],
-  webpackFinal: async (webpackConfig) => {
-    webpackConfig.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [require.resolve('@babel/preset-react')],
-          },
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-          options: {
-            // Provide the path to your tsconfig.json so that your stories can
-            // display types from outside each individual story.
-            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-          },
-        },
-      ],
-    });
-    webpackConfig.module.rules.push({
-      test: /\.stories\.tsx?$/,
-      loaders: [
-        {
-          loader: require.resolve('@storybook/source-loader'),
-          options: { parser: 'typescript' },
-        },
-      ],
-      enforce: 'pre',
-    });
-    webpackConfig.resolve.extensions.push('.ts', '.tsx');
-    return webpackConfig;
-  },
-
+  typescript: { reactDocgen: false },
   addons: [
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-actions/register',
     {
       name: '@storybook/addon-storysource',
       options: {
